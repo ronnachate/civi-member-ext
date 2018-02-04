f47433bd8f23029a7ee3fb9b82d88015397abf40
Ronnachate <ronnachate@gmail.com>
2018-02-04 23:44:59 +0700

    Add fk contribution id.

--- sql/auto_install.sql
+++ sql/auto_install.sql
@@ -1,18 +1,20 @@
-DROP TABLE IF EXISTS `civicrm_membership_period`;
 -- /*******************************************************
 -- *
 -- * civicrm_membership_period
 -- *
--- * Entity storing membership period record 
+-- * Entity storing membership period record
 -- *
 -- *******************************************************/
- CREATE TABLE `civicrm_membership_period` (
-      `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique MembershipPeriod ID',
-      `start_date` date    COMMENT 'Start date of membership period',
-      `end_date` date    COMMENT 'End date of membership period',
-      `membership_id` int unsigned    COMMENT 'FK to Membership',
-      `created_at` datetime    COMMENT 'End date of membership period',
+DROP TABLE IF EXISTS `civicrm_membership_period`;
 
-      PRIMARY KEY (`id`)
- ,    CONSTRAINT FK_civicrm_membership_period_membership_id FOREIGN KEY (`membership_id`) REFERENCES `civicrm_membership`(`id`) ON DELETE CASCADE
- )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
+CREATE TABLE `civicrm_membership_period` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique MembershipPeriod ID',
+    `start_date` date    COMMENT 'Start date of membership period',
+    `end_date` date    COMMENT 'End date of membership period',
+    `membership_id` int unsigned    COMMENT 'FK to Membership',
+    `contribution_id` int unsigned NULL   COMMENT 'FK to Contribution',
+    `created_at` datetime    COMMENT 'End date of membership period',
+    PRIMARY KEY (`id`),
+    CONSTRAINT FK_civicrm_membership_period_membership_id FOREIGN KEY (`membership_id`) REFERENCES `civicrm_membership`(`id`) ON DELETE CASCADE,
+    CONSTRAINT FK_civicrm_membership_period_contribution_id FOREIGN KEY (`contribution_id`) REFERENCES `civicrm_contribution`(`id`)
+)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
