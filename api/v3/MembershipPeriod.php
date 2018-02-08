f69e78276369947b597a190d241a52ec67ffb0d1
Ronnachate <ronnachate@gmail.com>
2018-02-08 12:41:09 +0700

    Fix create api.

--- api/v3/MembershipPeriod.php
+++ api/v3/MembershipPeriod.php
@@ -1,6 +1,8 @@
 <?php
 use CRM_Memberperiod_ExtensionUtil as E;
 
+define('MEMBERSHIP_PERIOD_ENTITY_NAME', 'MembershipPeriod', true);
+
 /**
  * MembershipPeriod.create API specification (optional)
  * This is used for documentation and validation.
@@ -10,7 +12,10 @@ use CRM_Memberperiod_ExtensionUtil as E;
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
  */
 function _civicrm_api3_membership_period_create_spec(&$spec) {
-  // $spec['some_parameter']['api.required'] = 1;
+    $spec['membership_id']['api.required'] = 1;
+    $spec['start_date']['api.required'] = 1;
+    $spec['end_date']['api.default'] = NULL;
+    $spec['created_at']['api.required'] = 1;
 }
 
 /**
@@ -21,7 +26,10 @@ function _civicrm_api3_membership_period_create_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_membership_period_create($params) {
-  return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+    $membership_period = CRM_Memberperiod_BAO_MembershipPeriod::createOrUpdate($membership_period_obj);
+    $period_array = array();
+    _civicrm_api3_object_to_array($membership_period, period_array[$membership_period->id]);
+    return civicrm_api3_create_success($period_array, $params, MEMBERSHIP_PERIOD_ENTITY_NAME, 'create');
 }
 
 /**
@@ -42,6 +50,7 @@ function civicrm_api3_membership_period_delete($params) {
  * @return array API result descriptor
  * @throws API_Exception
  */
+
 function civicrm_api3_membership_period_get($params) {
   return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
 }
