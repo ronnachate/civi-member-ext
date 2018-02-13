3223c97c1d5679640daffe5037ec8231d59dce6a
Ronnachate <ronnachate@gmail.com>
2018-02-13 23:44:12 +0700

    Fix schema with permament membership type.

--- sql/auto_install.sql
+++ sql/auto_install.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS `civicrm_membership_period`;
 CREATE TABLE `civicrm_membership_period` (
     `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique MembershipPeriod ID',
     `start_date` date    COMMENT 'Start date of membership period',
-    `end_date` date    COMMENT 'End date of membership period',
+    `end_date` date NULL   COMMENT 'End date of membership period',
     `membership_id` int unsigned    COMMENT 'FK to Membership',
     `contribution_id` int unsigned NULL   COMMENT 'FK to Contribution',
     `created_at` datetime    COMMENT 'End date of membership period',
